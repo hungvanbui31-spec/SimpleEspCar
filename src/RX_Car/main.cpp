9231348fbcc405ab177779c0c15cc111f030d7d8
imdolce <iamnotacracker@gmail.com>
2025-12-13 17:22:12 +0700

    modified:   README 	modified:   platformio.ini 	modified:   src/RX_Car/main.cpp 	modified:   src/TX_remote/main.cpp 	renamed:    test/ADS1115_test/ads.cpp -> test/test_ads1115/test_main.cpp 	renamed:    test/RX_receive_test/main.cpp -> test/test_rx/test_main.cpp 	renamed:    test/TX_sender_test/main.cpp -> test/test_tx/test_main.cpp

--- src/RX_Car/main.cpp
+++ src/RX_Car/main.cpp
@@ -2,35 +2,30 @@
 #include <espnow.h>
 #include <Arduino.h>
 
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+struct DataPacket {
+  int16_t adc0, adc1;
+  uint8_t status;
+};
+DataPacket packet;
 
-volatile bool newData = false;
-volatile uint8_t latestValue = 0;
+void onDataReceived(uint8_t *mac, uint8_t *data, uint8_t len) {
+  if (len == sizeof(DataPacket)) {
+    memcpy(&packet, data, sizeof(packet));
 
-void onDataReceived(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
-  if (len >= 1) {
-    latestValue = incomingData[0];
-    newData = true;
+    Serial.print("Value1: ");
+    Serial.println(packet.adc0);
+
+    Serial.print("Value2: ");
+    Serial.println(packet.adc1);
+
+    Serial.print("Status: ");
+    Serial.println(packet.status);
   }
 }
 
 void setup() {
   Serial.begin(115200);
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 20);
-  display.println("Waiting...");
-  display.display();
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -44,16 +39,4 @@ void setup() {
 }
 
 void loop() {
-  if (newData) {
-    newData = false;
-
-    Serial.print("Received: ");
-    Serial.println(latestValue);
-
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setCursor(0, 20);
-    display.print(latestValue);
-    display.display();
-  }
 }
--- src/TX_remote/main.cpp
+++ src/TX_remote/main.cpp
@@ -1,19 +1,58 @@
 #include <ESP8266WiFi.h>
+#include <Adafruit_ADS1X15.h>
 #include <Arduino.h>
 #include <espnow.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+// Define modules
+Adafruit_ADS1115 ads;
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+// -----------------------------------
 // Receiver MAC
 uint8_t receiverMAC[] = {0x24, 0xD7, 0xEB, 0xEB, 0xFA, 0x81};
 
-int valueToSend = 0;
+//int16_t adc0, adc1;
+
+const int boxSize = 50;                 // Size of the square in pixels
+const int boxX0 = (128  - boxSize) / 2;
+const int boxY0 = (64 - boxSize) / 2;
+
+struct DataPacket {
+  int16_t adc0, adc1;
+  uint8_t status;
+};
+DataPacket packet;
 
 void onSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Delivery Status: ");
   Serial.println(sendStatus == 0 ? "Success" : "Fail");
 }
 
+void drawJoystickPosition(int mappedX, int mappedY) {
+
+  display.clearDisplay();
+
+  // Draw bounding square
+  display.drawRect(boxX0, boxY0, boxSize, boxSize, SSD1306_WHITE);
+
+  // Draw the dot (use a small filled circle)
+  display.fillCircle(mappedX, mappedY, 2, SSD1306_WHITE);
+
+  display.display();
+}
+
 void setup() {
   Serial.begin(115200);
+  
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  Serial.println("SSD1306 allocation failed");
+  while (true);
+}
+
+  ads.begin();
+  ads.setGain(GAIN_TWOTHIRDS);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -32,18 +71,22 @@ void setup() {
 }
 
 void loop() {
-  // Send the integer value
-  uint8_t data = valueToSend;
-  esp_now_send(receiverMAC, &data, sizeof(data));
+  const int32_t ads_max = (5.03/6.144)*32768;
 
-  Serial.print("Sent: ");
-  Serial.println(valueToSend);
+  int16_t raw0 = ads.readADC_SingleEnded(0);
+  int16_t raw1 = ads.readADC_SingleEnded(1);
 
-  // Increase value
-  valueToSend++;
-  if (valueToSend > 100) {
-    valueToSend = 0;
-  }
+  packet.adc0 = map(raw0,0,(5.03/6.144)*32768,0,100);
+  packet.adc1 = map(raw1,0,(5.03/6.144)*32768,0,100);
+  packet.status  = (packet.adc0 > 50);
 
-  delay(200); // Adjust sending rate as needed
+  int posX = map(raw0,0,ads_max, boxX0, boxX0 + boxSize - 1);
+  int posY = map(raw1,0,ads_max, boxY0, boxY0 + boxSize - 1);
+
+  drawJoystickPosition(posX, posY);
+
+  esp_now_send(receiverMAC, (uint8_t*) &packet, sizeof(packet));
+
+  delay(50); // Adjust sending rate as needed
 }
+
